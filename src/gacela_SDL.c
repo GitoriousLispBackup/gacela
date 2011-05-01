dd2b7988d3aa135ae9247df2ca89274247ed5612
jsancho <devnull@localhost>
2011-05-01 15:37:31 +0000

    Mapeo de primeras funciones SDL, falta mirar cómo se hace el mapeo de las constantes y de las estructuras

--- src/gacela_SDL.c
+++ src/gacela_SDL.c
@@ -1,17 +1,29 @@
 #include <libguile.h>
 #include <SDL/SDL.h>
-#include <GL/gl.h>
 #include "gacela_SDL.h"
 
 SCM
 gacela_SDL_Init (SCM flags)
 {
-  return scm_make_integer (SDL_Init (scm_num2int (flags, "SDL_Init")));
+  return scm_int2num (SDL_Init (scm_num2int (flags, 0, "SDL_Init")));
+}
+
+SCM
+gacela_SDL_Quit ()
+{
+  SDL_Quit ();
+  return SCM_UNSPECIFIED;
+}
+
+SCM
+gacela_SDL_SetVideoMode (SCM width, SCM height, SCM bpp, SCM flags)
+{
 }
 
 void*
 SDL_register_functions (void* data)
 {
-  scm_c_define_gsubr ("SDL_Init", 1, 0, 0, &gacela_SDL_Init);
+  scm_c_define_gsubr ("SDL_Init", 1, 0, 0, gacela_SDL_Init);
+  scm_c_define_gsubr ("SDL_Quit", 0, 0, 0, gacela_SDL_Quit);
   return NULL;
 }
