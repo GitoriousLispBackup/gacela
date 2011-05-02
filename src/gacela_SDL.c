47e594acfc7f240288bf4e28156cc29395da763a
jsancho <devnull@localhost>
2011-05-02 08:46:29 +0000

    Ya puedo mapear constantes desde C a Scheme

--- src/gacela_SDL.c
+++ src/gacela_SDL.c
@@ -23,6 +23,7 @@ gacela_SDL_SetVideoMode (SCM width, SCM height, SCM bpp, SCM flags)
 void*
 SDL_register_functions (void* data)
 {
+  scm_c_define ("SDL_INIT_EVERYTHING", scm_int2num(SDL_INIT_EVERYTHING));
   scm_c_define_gsubr ("SDL_Init", 1, 0, 0, gacela_SDL_Init);
   scm_c_define_gsubr ("SDL_Quit", 0, 0, 0, gacela_SDL_Quit);
   return NULL;
