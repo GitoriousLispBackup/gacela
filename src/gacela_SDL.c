5d05f471fb4bd8eb8b3d3c0aaf6fdcae049be4b8
jsancho <devnull@localhost>
2011-05-05 06:16:59 +0000

    SDL_SetVideoMode funcionando

--- src/gacela_SDL.c
+++ src/gacela_SDL.c
@@ -18,6 +18,10 @@ gacela_SDL_Quit ()
 SCM
 gacela_SDL_SetVideoMode (SCM width, SCM height, SCM bpp, SCM flags)
 {
+  return scm_int2num (SDL_SetVideoMode (scm_num2int (width, 0, "SDL_SetVideoMode"), \
+					scm_num2int (height, 0, "SDL_SetVideoMde"), \
+					scm_num2int (bpp, 0, "SDL_SetVideoMde"), \
+					scm_num2int (flags, 0, "SDL_SetVideoMde")));
 }
 
 void*
@@ -46,5 +50,6 @@ SDL_register_functions (void* data)
 
   scm_c_define_gsubr ("SDL_Init", 1, 0, 0, gacela_SDL_Init);
   scm_c_define_gsubr ("SDL_Quit", 0, 0, 0, gacela_SDL_Quit);
+  scm_c_define_gsubr ("SDL_SetVideoMode", 4, 0, 0, gacela_SDL_SetVideoMode);
   return NULL;
 }
