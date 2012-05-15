8c11779c29d94a8c7702aae0964180252638dc10
Javier Sancho <jsf@jsancho.org>
2012-05-15 21:02:06 +0200

    Guybrush moves with cursors left and right

--- games/guybrush/guybrush.scm
+++ games/guybrush/guybrush.scm
@@ -10,10 +10,14 @@
 (define-mob (guybrush
 	     (guy (load-texture "guybrush.png"))
 	     (x 0)
-	     (y (- (/ (assoc-ref (get-game-properties) 'height) 2))))
+	     (y (+ (- (/ (assoc-ref (get-game-properties) 'height) 2))
+		   (/ (assoc-ref (get-texture-properties guy) 'height) 2))))
+
+  (cond ((key? 'left) (set! x (- x 10))))
+  (cond ((key? 'right) (set! x (+ x 10))))
+
   (translate x y)
   (draw-texture guy))
 
 (show-mob (guybrush))
 
-;; Tengo que hacer una función para obtener propiedades de textura en video.scm
